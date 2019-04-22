724672c26f5a95a20134dd04e079c8656bafcaff
sairim <spiri@protonmail.com>
2019-04-22 11:16:48 +0300

    set app up to use fetch properly + polyfills

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,6 +1,19 @@
+// check if we need to use polyfill
+if (!window.Promise) {
+    window.Promise = Promise;
+}
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js')
     .then(function() {
         console.log('serviceWorker registered')
     });
 }
+
+// customize when the install banner is shown
+window.addEventListener('beforeinstallprompt', function(event) {
+    console.log('beforeinstallprompt fired');
+    event.preventDefault();
+    deferredPrompt = event;
+    return false;
+})
--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,13 @@
+self.addEventListener('install', event => {
+    console.log('[serviceWorker] Installing service worker...', event);
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('[serviceWorker] Activating service worker...', event);
+    return self.clients.claim();
+});
+
 self.addEventListener('fetch', event => {
     console.log('[serviceWorker] Fetching something worker...', event);
     event.respondWith(fetch(event.request));
 });
-
-window.addEventListener('beforeinstallprompt', function(event) {
-    console.log('beforeinstallprompt fired');
-    event.preventDefault();
-    deferredPrompt = event;
-    return false;
-})
