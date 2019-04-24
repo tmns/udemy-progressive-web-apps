3640293ab2edfbf5e4a9ed0bc9d13eb3f17b1d97
sairim <spiri@protonmail.com>
2019-04-24 13:31:43 +0300

    add push notifiaction support

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -41,3 +41,84 @@ btnAdd2HS.addEventListener('click', event => {
             deferredPrompt = null;
         });
 });
+
+// notifications logic
+const enableNotificationButtons = document.querySelectorAll('.enable-notifications');
+
+function displayConfirmNotification() {
+    if ('serviceWorker' in navigator) {
+        const options = {
+            body: 'You successfully subscribed to our notification service!',
+            icon: '/src/images/icons/app-icon-96x96.png',
+            vibrate: [100, 50, 200],
+            badge: '/src/images/icons/app-icon-96x96.png',
+            tag: 'confirm-notification',
+            actions: [
+                { action: 'confirm', title: 'Okay', icon: '/src/images/icons/app-icon-96x96.png' },
+                { action: 'cancel', title: 'Cancel', icon: '/src/images/icons/app-icon-96x96.png' }
+            ]
+        }
+        navigator.serviceWorker.ready.then(function(swreg) {
+            swreg.showNotification('Successfully subscribed!', options);
+        })
+    }
+}
+
+function configurePushSub() {
+    if (!('serviceWorker' in navigator)) {
+        return;
+    }
+
+    let reg;
+    navigator.serviceWorker.ready.then(function(swreg) {
+        reg = swreg;
+        return swreg.pushManager.getSubscription();
+    }).then(function(sub) {
+        if (sub === null) {
+            // create a new sub
+            const vapidPubKey = 'BDV8RWTkrIu4ZOQ_NJCtNrS_E4nq9dsRxaYo3j72uP00GsyK8Z1_wZTZ90oN2fUeQVopOiTsLcH_dwseLEIfNQo';
+            const convertedVapidPubKey = urlBase64ToUint8Array(vapidPubKey);
+            return reg.pushManager.subscribe({
+                userVisibleOnly: true,
+                applicationServerKey: convertedVapidPubKey
+            });
+        } else {
+            // we already have one
+        }
+    }).then(function(newSub) {
+        return fetch('https://pwagram-6e256.firebaseio.com/subscriptions.json', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(newSub)
+        })
+    }).then(function(res) {
+        if (res.ok) {
+            displayConfirmNotification();
+        }
+    }).catch(function(err) {
+        console.log(err);
+    })
+
+}
+
+function askForNotificationPermission() {
+    Notification.requestPermission(function(result) {
+        console.log('User choice', result);
+        if (result !== 'granted') {
+            console.log('User did not grant notification permissions');
+        } else {
+            configurePushSub();
+            // displayConfirmNotification();
+        }
+    })
+}
+
+if ('Notification' in window && 'serviceWorker' in navigator) {
+    for (let i = 0; i < enableNotificationButtons.length; i++) {
+        enableNotificationButtons[i].style.display = 'block';
+        enableNotificationButtons[i].addEventListener('click', askForNotificationPermission);
+    }
+}
--- public/sw.js
+++ public/sw.js
@@ -19,7 +19,7 @@ const STATIC_FILES = [
     '/src/images/main-image.jpg',
     'https://fonts.googleapis.com/css?family=Roboto:400,700',
     'https://fonts.googleapis.com/icon?family=Material+Icons',
-    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'   
+    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
 ];
 
 // example function for enforcing a limit on the # of dynamically cached items
@@ -37,42 +37,42 @@ self.addEventListener('install', event => {
     console.log('[serviceWorker] Installing service worker...', event);
     // we use waitUntil to ensure cache is opened before we begin fetching
     event.waitUntil(caches.open(CACHE_STATIC_NAME)
-        .then(function(cache) {
+        .then(function (cache) {
             console.log('[Service Worker] Precaching App Shell');
             cache.addAll(STATIC_FILES);
         })
-    ) 
+    )
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
     console.log('[serviceWorker] Activating service worker...', event);
     event.waitUntil(
-        caches.keys().then(function(keyList) {
-            return Promise.all(keyList.map(function(key) {
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
                 if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
                     console.log('[Service Worker] Removing old cache:', key);
                     return caches.delete(key);
-                } 
+                }
             }));
         })
     );
     return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
     // network only strategy for specific route here
     const url = 'https://pwagram-6e256.firebaseio.com/posts';
     if (event.request.url.indexOf(url) > -1) {
         event.respondWith(
             // open & store in our dynamic cache
             // caches.open(CACHE_DYNAMIC_NAME).then(function(cache) {
-            fetch(event.request).then(function(res) {
+            fetch(event.request).then(function (res) {
                 // trimCache(CACHE_DYNAMIC_NAME, 20);
                 // cache.put(event.request, res.clone());
                 const clonedRes = res.clone();
-                clearAllData('posts').then(function() {
-                    return clonedRes.json();                    
-                }).then(function(data) {
+                clearAllData('posts').then(function () {
+                    return clonedRes.json();
+                }).then(function (data) {
                     for (const key in data) {
                         writeData('posts', data[key]);
                     }
@@ -86,38 +86,38 @@ self.addEventListener('fetch', function(event) {
     } else {
         // cache then network strategy here
         event.respondWith(
-            caches.match(event.request).then(function(response) {
+            caches.match(event.request).then(function (response) {
                 if (response) {
                     return response;
                 } else {
-                    return fetch(event.request).then(function(res) {
+                    return fetch(event.request).then(function (res) {
                         // open & store in our dynamic cache
-                        caches.open(CACHE_DYNAMIC_NAME).then(function(cache) {
+                        caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
                             // trimCache(CACHE_DYNAMIC_NAME, 20);
                             cache.put(event.request.url, res.clone());
                             return res;
                         })
                     })
-                    .catch(function(err) {
-                        // if we're offline and the user requests text/html resource, serve /offline.html
-                        return caches.open(CACHE_STATIC_NAME).then(function(cache) {
-                            if (event.request.headers.get('accept').includes('text/html')) {
-                                return cache.match('/offline.html');
-                            }
+                        .catch(function (err) {
+                            // if we're offline and the user requests text/html resource, serve /offline.html
+                            return caches.open(CACHE_STATIC_NAME).then(function (cache) {
+                                if (event.request.headers.get('accept').includes('text/html')) {
+                                    return cache.match('/offline.html');
+                                }
+                            })
                         })
-                    })
                 }
             })
         )
     }
 });
 
-self.addEventListener('sync', function(event) {
+self.addEventListener('sync', function (event) {
     console.log('[Service Worker] Background syncing', event);
     if (event.tag === 'sync-new-posts') {
         console.log('[Service Worker] Syncing new posts...');
         event.waitUntil(
-            readAllData('sync-posts').then(function(data) {
+            readAllData('sync-posts').then(function (data) {
                 for (const dt of data) {
                     fetch('https://us-central1-pwagram-6e256.cloudfunctions.net/storePostData', {
                         method: 'POST',
@@ -131,14 +131,14 @@ self.addEventListener('sync', function(event) {
                             location: dt.location,
                             image: 'https://firebasestorage.googleapis.com/v0/b/pwagram-6e256.appspot.com/o/sf-boat.jpg?alt=media&token=b9580ef9-e0f6-4e89-bdf1-735b1be6e91e'
                         })
-                    }).then(function(res) {
+                    }).then(function (res) {
                         console.log('Sent data', res);
                         if (res.ok) {
-                            res.json().then(function(resData) {
+                            res.json().then(function (resData) {
                                 deleteItemFromData('sync-posts', resData.id);
                             })
                         }
-                    }).catch(function(err) {
+                    }).catch(function (err) {
                         console.log('Error while sending data', err);
                     })
                 }
@@ -146,3 +146,58 @@ self.addEventListener('sync', function(event) {
         );
     }
 });
+
+self.addEventListener('notificationclick', function (event) {
+    const notification = event.notification;
+    const action = event.action;
+
+    console.log(notification);
+
+    if (action === 'confirm') {
+        console.log('confirm was chosen');
+    } else {
+        console.log(action);
+        event.waitUntil(
+            clients.matchAll().then(function(clis) {
+                const client = clis.find(function(c) {
+                    return c.visibilityState === 'visible';
+                });
+                
+                if (client !== undefined) {
+                    client.navigate(notification.data.url);
+                    client.focus();
+                } else {
+                    clients.openWindow(notification.data.url);
+                }
+
+                notification.close();
+            })
+        )
+    }
+})
+
+self.addEventListener('notificationclose', function (event) {
+    console.log('notification was closed', event);
+})
+
+self.addEventListener('push', function (event) {
+    console.log('push notification received', event);
+
+    let data = { title: 'Fallback Message', content: 'Something new happened!', openURL: '/help' };
+    if (event.data) {
+        data = JSON.parse(event.data.text()); 
+    }
+
+    const options = {
+        body: data.content,
+        icon: '/src/images/icons/apple-icon-96x96.png',
+        badge: '/src/images/icons/apple-icon-96x96.png',
+        data: {
+            url: data.openURL
+        }
+    } 
+
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+})
